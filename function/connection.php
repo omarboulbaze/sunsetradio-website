8c57b632c1d5ba570573dac2160a70125e5daddf
KruserG <yokariig@gmail.com>
2020-07-28 06:07:37 -0400

    Update connection.php

--- function/connection.php
+++ function/connection.php
@@ -4,9 +4,9 @@
 // Variables for connection
 
 $localhost = 'localhost';
-$username = 'root'; 
+$username = 'sunsetra_sunset'; 
 $password = 'rg^EchG^4N7A'; //rg^EchG^4N7A
-$dbname = 'sunset'; 
+$dbname = 'sunsetra_sunset'; 
 
 $connect = mysqli_connect($localhost, $username, $password,$dbname) or die ("Erreur de connexion : " . mysqli_connect_error());
 
