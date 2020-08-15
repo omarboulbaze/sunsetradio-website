d1494738a1820c8714a1b2a918bbdb8319b69a88
Jason Kruser <yokariig@gmail.com>
2020-08-15 05:45:09 -0400

    adding app.php page

---
+++ app.php
@@ -0,0 +1,86 @@
+
+<html>
+
+<head>
+
+<meta charset="utf-8" />
+
+<!-- Google Analytics for stats only -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E8KR9F8F1P"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E8KR9F8F1P');
+</script>
+
+<!-- OG META -->
+<meta property="og:site_name" content="www.sunsetradio.me">
+<meta property="og:url" content="https://www.sunsetradio.me/">
+<meta property="og:title" content="Sunset Radio">
+<meta property="og:description" content="Sunset Radio, la radio 2.0 ! Rejoignez la communauté sur Discord et intéragissez avec les animateurs EN DIRECT !">
+<meta property="og:type" content="website">
+<meta name="og:image" content="https://i.imgur.com/DwtzhmQ.png">
+
+<!-- STYLES -->
+<link rel="stylesheet" type="text/css" href="./abootstrap/bootstrap.min.css"/>
+<link href="./afontawesome/css/all.css" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="./style/style.css"/>
+
+<!-- FAVICON -->
+<link rel="shortcut icon" type="image/png" href="https://i.imgur.com/DwtzhmQ.png"/>
+
+<!-- FONTS -->
+<link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+
+<!-- TITLE -->
+<title>Sunset Radio - ÇA ARRIVE</title>
+  
+</head>
+
+  <body class="bgRadio">
+   
+  
+  <!-- NAV BAR -->
+  
+  <nav class="navbar navbar-expand-lg barreNavSR big navbar-dark bg-dark">
+
+  <a class="navbar-brand" href="index.php">
+
+    <img src="https://i.imgur.com/DwtzhmQ.png" width="75" height="75" alt="SunsetRadio" loading="lazy">
+  </a>
+  
+
+ 
+
+
+
+
+  <a href="https://discord.com/invite/KSARukG" target="_blank"><button class="btn btn-outline-light my-2 my-sm-0 big colorSR mx-1"><i class="fab fa-discord"></i> Discord</button></a>
+ 
+
+</nav>
+  <!-- NAV BAR END -->
+
+
+
+
+
+
+<div class="container mt-5">
+<div class="d-flex justify-content-center">
+<iframe src="https://www.radioking.com/widgets/player/player.php?id=330331&c=%232C2A46&c2=%23F3C980&f=v&i=0&ii=null&p=1&s=0&li=0&popup=0&plc=0&h=371&l=undefined&a=0&v=2" style="border-radius: 5px; width: 275px; height: 371px; min-width: 0; min-height: 365px; -webkit-box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.3);-moz-box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.3);box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.3);" frameBorder="0" ></iframe>
+</div>
+
+
+
+<!-- Javascript -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="./script/script.js"></script>
+
+</body>
+</html>
